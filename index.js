7214b012342f9b2ba747febb19a034c53c44689f
nadiianazarchuk@gmail.com <nadiianazarchuk@gmail.com>
2019-10-15 05:06:55 +0300

    html & css corrections were done

--- index.js
+++ index.js
@@ -4,9 +4,10 @@
 const header = document.querySelector( '.header' );
 
 window.onscroll = () => {
-  if (window.pageYOffset > 1) {
+  if (window.pageYOffset > 60) {
       header.classList.add("header__reduce");
   } else {
       header.classList.remove("header__reduce");
   }
  };
+
