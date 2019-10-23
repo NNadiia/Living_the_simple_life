966d23490dcc40bbfe8ec91cd8547de296f63785
nadiianazarchuk@gmail.com <nadiianazarchuk@gmail.com>
2019-10-23 03:16:39 +0300

    css and js were corrected

--- index.js
+++ index.js
@@ -4,10 +4,17 @@
 const header = document.querySelector( '.header' );
 
 window.onscroll = () => {
-  if (window.pageYOffset > 60) {
-      header.classList.add("header__reduce");
-  } else {
-      header.classList.remove("header__reduce");
-  }
- };
-
+    if (window.pageYOffset > 265) {
+        if (!header.classList.contains('header__reduce')) {
+            header.classList.add('header__reduce');
+        }
+    }
+    else if (window.pageYOffset < 60) {
+        if (header.classList.contains('header__reduce')) {
+            header.classList.remove('header__reduce');
+        }
+    }
+    else {
+        return header;
+    }
+};
