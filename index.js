5fd9d0c1d228bb439629d70e0526e633a5a8044f
nadiianazarchuk@gmail.com <nadiianazarchuk@gmail.com>
2019-11-11 03:04:24 +0200

    header_reduce for @media was corrected

--- index.js
+++ index.js
@@ -4,17 +4,34 @@
 const header = document.querySelector( '.header' );
 
 window.onscroll = () => {
-    if (window.pageYOffset > 265) {
-        if (!header.classList.contains('header__reduce')) {
-            header.classList.add('header__reduce');
+    if (window.innerWidth > 950) {
+        if (window.pageYOffset > 265) {
+            if (!header.classList.contains('header__reduce')) {
+                header.classList.add('header__reduce');
+            }
         }
-    }
-    else if (window.pageYOffset < 60) {
-        if (header.classList.contains('header__reduce')) {
-            header.classList.remove('header__reduce');
+        else if (window.pageYOffset < 60) {
+            if (header.classList.contains('header__reduce')) {
+                header.classList.remove('header__reduce');
+            }
+        }
+        else {
+            return header;
         }
     }
     else {
-        return header;
+        if (window.pageYOffset > 330) {
+            if (!header.classList.contains('header__reduce')) {
+                header.classList.add('header__reduce');
+            }
+        }
+        else if (window.pageYOffset < 250) {
+            if (header.classList.contains('header__reduce')) {
+                header.classList.remove('header__reduce');
+            }
+        }
+        else {
+            return header;
+        }
     }
 };
